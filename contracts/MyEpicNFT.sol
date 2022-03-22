852a46ad9e8306eed91d21f1c4bb39f206e85156
Noelia Belen Lopez <noelia.l@toptal.com>
2022-03-22 18:41:22 +0100

    Counters wip

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,9 +1,29 @@
 pragma solidity ^0.8.1;
 
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("This is my NFT contract. Whoa!");
-    }
+contract MyEpicNFT is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // We need to pass the name of our NFTs token and its symbol.
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contract. Woah!");
+  }
+
+  function makeAnEpicNFT() public {
+    uint256 newItemId = _tokenIds.current();
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender, newItemId);
+
+    // Set the NFTs data.
+    _setTokenURI(newItemId, "blah");
+
+    // Increment the counter for when the next NFT is minted.
+    _tokenIds.increment();
+  }
 }
